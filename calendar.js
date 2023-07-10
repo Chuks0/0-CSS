09d10ec94294c6e7a3805e4cf2f10fc7980a07ac
Chuks <ozodic@gmail.com>
2023-07-10 18:24:36 +0900

    rename

--- calendar.js
+++ calendar.js
@@ -3,23 +3,10 @@ const calendar = document.querySelector(".calendar"),
     daysContainer = document.querySelector(".days"),
     prev = document.querySelector(".prev"),
     next = document.querySelector(".next"),
-    todayBtn = document.querySelector(".today-btn"),
-    gotoBtn = document.querySelector(".goto-btn"),
-    dateInput = document.querySelector(".date-input"),
-    //eventDay = document.querySelector(".event-day"),
-    //eventDay = document.querySelector(".calendar-day"),
-    eventDate = document.querySelector(".event-date"),
+    // todayBtn = document.querySelector(".today-btn"),
     eventsContainer = document.querySelector(".calendar-slots-wrapper"),
-    addEventBtn = document.querySelector(".add-event"),
-    addEventWrapper = document.querySelector(".add-event-wrapper "),
-    addEventCloseBtn = document.querySelector(".close "),
-    addEventTitle = document.querySelector(".event-name "),
-    addEventFrom = document.querySelector(".event-time-from "),
-    addEventTo = document.querySelector(".event-time-to "),
-    //selectDate = document.querySelector(".today-date"),
     calendarDays = document.querySelectorAll(".calendar-day"),
-    calendarTimes = document.querySelectorAll(".calendar-time-slot-terms"),
-    addEventSubmit = document.querySelector(".add-event-btn ");
+    calendarTimes = document.querySelectorAll(".calendar-time-slot-terms");
 
 let today = new Date();
 let activeDay;
@@ -196,53 +183,12 @@ function addListner() {
     });
 }
 
-todayBtn.addEventListener("click", () => {
-    today = new Date();
-    month = today.getMonth();
-    year = today.getFullYear();
-    initCalendar();
-});
-
-dateInput.addEventListener("input", (e) => {
-    dateInput.value = dateInput.value.replace(/[^0-9/]/g, "");
-    if (dateInput.value.length === 2) {
-        dateInput.value += "/";
-    }
-    if (dateInput.value.length > 7) {
-        dateInput.value = dateInput.value.slice(0, 7);
-    }
-    if (e.inputType === "deleteContentBackward") {
-        if (dateInput.value.length === 3) {
-            dateInput.value = dateInput.value.slice(0, 2);
-        }
-    }
-});
-
-gotoBtn.addEventListener("click", gotoDate);
-
-function gotoDate() {
-    console.log("here");
-    const dateArr = dateInput.value.split("/");
-    if (dateArr.length === 2) {
-        if (dateArr[0] > 0 && dateArr[0] < 13 && dateArr[1].length === 4) {
-            month = dateArr[0] - 1;
-            year = dateArr[1];
-            initCalendar();
-            return;
-        }
-    }
-    alert("Invalid Date");
-}
-
-//function get active day day name and date and update eventday eventdate
-/*function getActiveDay(date) {
-    const day = new Date(year, month, date);
-    console.log(day);
-    const dayName = day.toString().split(" ")[0];
-    console.log(dayName);
-    eventDay.innerHTML = dayName;
-    eventDate.innerHTML = date + " " + months[month] + " " + year;
-}*/
+// todayBtn.addEventListener("click", () => {
+//     today = new Date();
+//     month = today.getMonth();
+//     year = today.getFullYear();
+//     initCalendar();
+// });
 
 function setWeekSlotArr() {
     let results = [];
@@ -511,191 +457,6 @@ function dateCheck(day, i, end, e) {
     //console.log(JSON.stringify(weekSlotsArr));
     //console.log(minSetVal);
 }
-
-//function to add event
-/*addEventBtn.addEventListener("click", () => {
-    addEventWrapper.classList.toggle("active");
-});*/
-
-/*addEventCloseBtn.addEventListener("click", () => {
-    addEventWrapper.classList.remove("active");
-});*/
-
-/*document.addEventListener("click", (e) => {
-    if (e.target !== addEventBtn && !addEventWrapper.contains(e.target)) {
-        addEventWrapper.classList.remove("active");
-    }
-});*/
-
-//allow 50 chars in eventtitle
-//addEventTitle.addEventListener("input", (e) => {
-//    addEventTitle.value = addEventTitle.value.slice(0, 60);
-//});
-
-function defineProperty() {
-    var osccred = document.createElement("div");
-    osccred.innerHTML =
-        "A Project By <a href='https://www.youtube.com/channel/UCiUtBDVaSmMGKxg1HYeK-BQ' target=_blank>Open Source Coding</a>";
-    osccred.style.position = "absolute";
-    osccred.style.bottom = "0";
-    osccred.style.right = "0";
-    osccred.style.fontSize = "10px";
-    osccred.style.color = "#ccc";
-    osccred.style.fontFamily = "sans-serif";
-    osccred.style.padding = "5px";
-    osccred.style.background = "#fff";
-    osccred.style.borderTopLeftRadius = "5px";
-    osccred.style.borderBottomRightRadius = "5px";
-    osccred.style.boxShadow = "0 0 5px #ccc";
-    document.body.appendChild(osccred);
-}
-
-//defineProperty();
-
-//allow only time in eventtime from and to
-/*addEventFrom.addEventListener("input", (e) => {
-    addEventFrom.value = addEventFrom.value.replace(/[^0-9:]/g, "");
-    if (addEventFrom.value.length === 2) {
-        addEventFrom.value += ":";
-    }
-    if (addEventFrom.value.length > 5) {
-        addEventFrom.value = addEventFrom.value.slice(0, 5);
-    }
-});
-
-addEventTo.addEventListener("input", (e) => {
-    addEventTo.value = addEventTo.value.replace(/[^0-9:]/g, "");
-    if (addEventTo.value.length === 2) {
-        addEventTo.value += ":";
-    }
-    if (addEventTo.value.length > 5) {
-        addEventTo.value = addEventTo.value.slice(0, 5);
-    }
-});*/
-
-//function to add event to eventsArr
-/*addEventSubmit.addEventListener("click", () => {
-    const eventTitle = addEventTitle.value;
-    const eventTimeFrom = addEventFrom.value;
-    const eventTimeTo = addEventTo.value;
-    if (eventTitle === "" || eventTimeFrom === "" || eventTimeTo === "") {
-        alert("Please fill all the fields");
-        return;
-    }
-
-    //check correct time format 24 hour
-    const timeFromArr = eventTimeFrom.split(":");
-    const timeToArr = eventTimeTo.split(":");
-    if (
-        timeFromArr.length !== 2 ||
-        timeToArr.length !== 2 ||
-        timeFromArr[0] > 23 ||
-        timeFromArr[1] > 59 ||
-        timeToArr[0] > 23 ||
-        timeToArr[1] > 59
-    ) {
-        alert("Invalid Time Format");
-        return;
-    }
-
-    const timeFrom = convertTime(eventTimeFrom);
-    const timeTo = convertTime(eventTimeTo);
-
-    //check if event is already added
-    let eventExist = false;
-    eventsArr.forEach((event) => {
-        if (
-            event.day === activeDay &&
-            event.month === month + 1 &&
-            event.year === year
-        ) {
-            event.events.forEach((event) => {
-                if (event.title === eventTitle) {
-                    eventExist = true;
-                }
-            });
-        }
-    });
-    if (eventExist) {
-        alert("Event already added");
-        return;
-    }
-    const newEvent = {
-        title: eventTitle,
-        time: timeFrom + " - " + timeTo,
-        status: "available",
-    };
-    console.log(newEvent);
-    console.log(activeDay);
-    let eventAdded = false;
-    if (eventsArr.length > 0) {
-        eventsArr.forEach((item) => {
-            if (
-                item.day === activeDay &&
-                item.month === month + 1 &&
-                item.year === year
-            ) {
-                item.events.push(newEvent);
-                eventAdded = true;
-            }
-        });
-    }
-
-    if (!eventAdded) {
-        eventsArr.push({
-            day: activeDay,
-            month: month + 1,
-            year: year,
-            events: [newEvent],
-        });
-    }
-
-    console.log(eventsArr);
-    addEventWrapper.classList.remove("active");
-    addEventTitle.value = "";
-    addEventFrom.value = "";
-    addEventTo.value = "";
-    //updateEvents(activeDay);
-    //select active day and add event class if not added
-    const activeDayEl = document.querySelector(".day.active");
-    if (!activeDayEl.classList.contains("event")) {
-        activeDayEl.classList.add("event");
-    }
-});*/
-
-//function to delete event when clicked on event
-/*eventsContainer.addEventListener("click", (e) => {
-    if (e.target.classList.contains("event")) {
-        //if client book the time
-        if (confirm("Are you sure you want to delete this event?")) {
-            const eventTitle = e.target.children[0].children[1].innerHTML;
-            eventsArr.forEach((event) => {
-                if (
-                    event.day === activeDay &&
-                    event.month === month + 1 &&
-                    event.year === year
-                ) {
-                    event.events.forEach((item, index) => {
-                        if (item.title === eventTitle) {
-                            event.events.splice(index, 1);
-                        }
-                    });
-                    //if no events left in a day then remove that day from eventsArr
-                    if (event.events.length === 0) {
-                        eventsArr.splice(eventsArr.indexOf(event), 1);
-                        //remove event class from day
-                        const activeDayEl =
-                            document.querySelector(".day.active");
-                        if (activeDayEl.classList.contains("event")) {
-                            activeDayEl.classList.remove("event");
-                        }
-                    }
-                }
-            });
-            //updateEvents(activeDay);
-        }
-    }
-});*/
 
 //function to save events in local storage
 function saveEvents() {
